5a30f0005dfe60549f8b3500ab4ae7608b3c2f03
roundedrectangle <kozelderezel12345678@gmail.com>
2025-05-17 19:30:54 +0300

    add sha256 function

--- pyotherside_utils/__init__.py
+++ pyotherside_utils/__init__.py
@@ -1,6 +1,11 @@
 from __future__ import annotations
 
+import hashlib
+
 from .sending import *
 from .errors import *
 from .network import *
 from .configbase import *
+
+def sha256(data: str):
+    return hashlib.sha256(data.encode()).hexdigest()
