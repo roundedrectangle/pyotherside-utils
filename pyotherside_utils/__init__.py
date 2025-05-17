d82f43a89cb05f4ffe47096178367da43de284b6
roundedrectangle <kozelderezel12345678@gmail.com>
2025-05-17 08:45:54 +0300

    add other functions from sailcord

--- pyotherside_utils/__init__.py
+++ pyotherside_utils/__init__.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+import asyncio
 from typing import TYPE_CHECKING
 import urllib.parse
 from datetime import datetime
+from contextlib import suppress
 
 qsend = lambda *args: print(*args) # Fallback for testing
 if not TYPE_CHECKING:
@@ -25,3 +27,14 @@ def convert_proxy(proxy):
 def qml_date(date: datetime):
     """Convert to UTC Unix timestamp using milliseconds"""
     return date.timestamp()*1000
+
+async def cancel_gen(agen):
+    task = asyncio.create_task(agen.__anext__())
+    task.cancel()
+    with suppress(asyncio.CancelledError):
+        await task
+    await agen.aclose()
+
+def isurl(obj: str):
+    """Returns True if an object is an internet URL"""
+    return urllib.parse.urlparse(obj).scheme != '' #not in ('file','')
