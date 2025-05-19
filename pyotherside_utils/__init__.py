b475befc1af025d5312f7c580e9bee9171448d78
roundedrectangle <kozelderezel12345678@gmail.com>
2025-05-19 20:37:58 +0300

    move hashing in a separate file

--- pyotherside_utils/__init__.py
+++ pyotherside_utils/__init__.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import hashlib
-
 from .sending import *
 from .errors import *
 from .network import *
@@ -10,6 +8,4 @@ from .configbase import *
 from .temporarymanager import *
 from .fs import *
 from .caching import *
-
-def sha256(data: str):
-    return hashlib.sha256(data.encode()).hexdigest()
+from .hashing import *
