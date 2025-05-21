2ef6746cc5c8636469c2da8f383cb1278571162a
roundedrectangle <kozelderezel12345678@gmail.com>
2025-05-21 21:07:35 +0300

    support requests and httpx for downloading

--- pyotherside_utils/network.py
+++ pyotherside_utils/network.py
@@ -1,17 +1,30 @@
 from __future__ import annotations
 
 import os, shutil
-import re
 from pathlib import Path
+from typing import TYPE_CHECKING, Iterator
 import urllib.parse, urllib.request, urllib.error
 
-from . import show_error
+try:
+    import httpx
+    HTTPX_AVAILABLE = True
+except: HTTPX_AVAILABLE = False
+
+try:
+    import requests
+    REQUESTS_AVAILABLE = True
+except: REQUESTS_AVAILABLE = False
+
+if TYPE_CHECKING:
+    import httpx, requests
+
+from . import show_error, exception_safe, ExceptionHandlingInfo, DataFromException
 
 def generate_headers(user_agent=None):
     return {
         "User-Agent": "python-requests/2.32.3" if user_agent is None else user_agent,
-        "Accept-Encoding": "gzip,deflate",
-        "Accept": "*/*",
+        #"Accept-Encoding": "gzip,deflate",
+        #"Accept": "*/*",
         "Connection": "keep-alive",
     }
 
@@ -30,9 +43,9 @@ def isurl(obj: str):
     """Returns True if an object is an internet URL"""
     return urllib.parse.urlparse(obj).scheme != '' #not in ('file','')
 
-DOWNLOAD_CHUNK_SIZE = 1024
+DOWNLOAD_CHUNK_SIZE = 1024*1024 # 1mb
 
-def download(url, proxies=None, user_agent=None):
+def stream_urllib(url, proxies=None, user_agent=None):
     """Returns same as urllib.request.urlopen() or None if URL is invalid"""
     try:
         opener = urllib.request.build_opener(urllib.request.ProxyHandler(proxies))
@@ -44,13 +57,63 @@ def download(url, proxies=None, user_agent=None):
     except Exception as e:
         show_error('cache', f'{type(e)}: {e}')
 
-def download_save(url, destination: Path | str, proxies: dict | None = None, user_agent: str | None = None):
-    r = download(url, proxies, user_agent)
-    if r:
-        with open(destination, 'wb') as f:
-            shutil.copyfileobj(r, f, DOWNLOAD_CHUNK_SIZE)
+def stream_httpx(url, proxies=None, client: httpx.Client | None = None, user_agent=None):
+    headers = {'User-Agent': user_agent} if user_agent is not None else None
+    if client:
+        context = client.stream('get', url, headers=headers)
+    elif HTTPX_AVAILABLE:
+        context = httpx.stream('get', url, headers=headers, proxy=proxies)
+    else: return
+    with context as r:
+        for chunk in r.iter_bytes(DOWNLOAD_CHUNK_SIZE):
+            yield chunk
+
+def stream_requests(url, proxies=None, user_agent=None):
+    headers = {'User-Agent': user_agent} if user_agent is not None else None
+    if REQUESTS_AVAILABLE:
+        try: r = requests.get(url, stream=True, proxies=proxies, headers=headers)
+        except requests.ConnectionError as e:
+            show_error('cacheConnection', str(e))
+            return
+        except requests.RequestException as e:
+            show_error('cache', str(type(e)), str(e))
+            return
+        if r.status_code == 200:
+            return iter(r)
+
+if HTTPX_AVAILABLE:
+    stream_httpx = exception_safe({httpx.HTTPError: ExceptionHandlingInfo('cacheConnection', DataFromException.str_exception)})(stream_httpx) # pyright:ignore[reportAssignmentType]
+stream_httpx = exception_safe({Exception: ExceptionHandlingInfo('cache', lambda e: f'{type(e)}: {e}')})(stream_httpx) # pyright:ignore[reportAssignmentType]
+
+def save_iterator(iterator: Iterator[bytes | str] | None, destination: Path | str):
+    os.remove(destination)
+    if iterator:
+        for chunk in iterator:
+            with open(destination, 'a' + ('b' if isinstance(chunk, bytes) else '')) as f:
+                f.write(chunk)
         return True
     return False
+
+def save_file_like(file, destination: Path | str):
+    if file:
+        with open(destination, 'wb') as f:
+            shutil.copyfileobj(file, f, DOWNLOAD_CHUNK_SIZE)
+        return True
+    return False
+
+def download_save(
+    url,
+    destination: Path | str,
+    proxies: dict | None = None,
+    user_agent: str | None = None,
+    httpx_client: httpx.Client | None = None,
+):
+    if httpx_client or HTTPX_AVAILABLE:
+        return save_iterator(stream_httpx(url, proxies, httpx_client, user_agent), destination)
+    elif REQUESTS_AVAILABLE:
+        return save_iterator(stream_requests(url, proxies, user_agent), destination)
+    else:
+        return save_file_like(stream_urllib(url, proxies, user_agent), destination)
 
 def get_extension_from_url(url: str, default='png'):
     res = os.path.splitext(urllib.parse.urlparse(url).path)[1]
