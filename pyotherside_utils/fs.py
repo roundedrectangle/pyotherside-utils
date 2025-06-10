7bd09ae916a70c21ab0d606b4e97514ae3f2f04c
roundedrectangle <kozelderezel12345678@gmail.com>
2025-06-10 21:53:27 +0300

    fs: add autoincremented_file_path

--- pyotherside_utils/fs.py
+++ pyotherside_utils/fs.py
@@ -37,3 +37,15 @@ def find_extracted_contents(path: Path | str):
     return path
 
 find_contents = find_extracted_contents # deprecated FIXME: remove this when i'll work on Utilities or whatever uses this
+
+#AUTOINCREMENTED_FILE_PATH_RE = re.compile(r'^.* \([1-9][0-9]*\)$')
+def autoincremented_file_path(path: Path | str):
+    path = Path(path)
+    i = 2
+    if path.exists():
+        path = path.with_stem(f'{path.stem} (2)')
+    i += 1 # So if while loop is entered it will properly update path to be 3 instead of 2 and substract i-1 length without doing this 2 times
+    while path.exists():
+        path = path.with_stem(path.stem[:-(3 + len(str(i-1)))] + f' ({i})')
+        i += 1
+    return path
